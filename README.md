967ccd617ca42eaa03a96e0a1a703d0ca565b040
Carlos Salgado <socd06@gmail.com>
2019-06-14 23:13:12 -0400

    Updated variable names and comments.

---
+++ README.md
@@ -0,0 +1,17 @@
+Writen in C POSIX.
+Software used:
+- MPLAB IDE
+- RT-PReempt Linux Kernel
+- PICC C Compiler
+- Minicom (Linux)
+- HyperTerminal (Windows debugging)
+Hardware used:
+- PIC16F887 PIC microcontroller
+- MAX232A TTL IC
+- 2N6507 Sillicon-Controlled Rectifier
+- MOC3011 TRIAC
+- LM35 Thermistor
+- PICKit 2
+- Polarization circuits in protoboard
+- PCB for power circuit
+- Null-modem RS232 cable
--- RTOSHVAC2011.c
+++ RTOSHVAC2011.c
@@ -7,39 +7,39 @@
 #include <stdlib.h>
 #include <sys/io.h>
 
-#define paralelo 0x378 //Paralell port address
+#define parallel 0x378 //Parallel port address
 
 struct variables{
       int fd; 
-      char *datos;
-      int tamanio; 
-      int tiempo;
+      char *data;
+      int ssize; 
+      int time;
       };
 struct variables v;
 struct timespec next;
 float t=0;
-v.tamanio=1; 
-v.tiempo=500000;
+v.ssize=1; 
+v.time=500000;
 
-int *hilo_serie(void *args){
+int *tthread_serie(void *args){
    clock_gettime(CLOCK_REALTIME, &next);
    next.tv_sec = next.tv_sec + 0.05; // otros 5ms
    next.tv_nsec = 0;
    v.fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY);
    if (v.fd == -1)
       { 
-      perror("\n ERROR: No es posible abrir el puerto. /dev/ttyS0 - \n");
+      perror("\n ERROR: Unable to open port. /dev/ttyS0 - \n");
       }
       else
 	fcntl(v.fd, F_SETFL, 0);
-      printf("\r   Puerto abierto... id: %d \n",v.fd);
+      printf("\r   Port open... id: %d \n",v.fd);
       return (v.fd);
     }
 
-void *hilo_config(void *args){
+void *tthread_config(void *args){
    struct timespec next;
    clock_gettime(CLOCK_REALTIME, &next);
-   next.tv_sec = next.tv_sec + 0.05; // 5 miliseg
+   next.tv_sec = next.tv_sec + 0.05; // 5 ms
    next.tv_nsec = 0;
    clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &next, NULL);
    struct termios options;
@@ -47,21 +47,21 @@ void *hilo_config(void *args){
    cfsetispeed(&options, B300);
    cfsetospeed(&options, B300);
    options.c_cflag |= (CLOCAL | CREAD);
-   options.c_cflag &= ~CSIZE; 
+   options.c_cflag &= ~Cssize; 
    options.c_cflag |= CS8;    
    options.c_cflag &= ~PARENB;
    options.c_cflag &= ~CSTOPB;
-   options.c_cflag &= ~CSIZE;
+   options.c_cflag &= ~Cssize;
    options.c_cflag |= CS8;
    options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
    tcsetattr(v.fd, TCSANOW, &options);
-   printf("\r   Puerto configurado exitosamente\n");
+   printf("\r   Port configured succesfully\n");
    }
 
-int *hilo_leer(int serial_fd, char *data, int size, int timeout_usec){
+int *tthread_leer(int serial_fd, char *data, int ssize, int timeout_usec){
    struct timespec next;
    clock_gettime(CLOCK_REALTIME, &next);
-   next.tv_sec = next.tv_sec + 0.1; // 100 miliseg
+   next.tv_sec = next.tv_sec + 0.1; // 100 ms
    next.tv_nsec = 0;
    clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &next, NULL);
    fd_set fds;
@@ -74,23 +74,23 @@ int *hilo_leer(int serial_fd, char *data, int size, int timeout_usec){
       FD_SET (serial_fd, &fds);
       timeout.tv_sec = 0;  
       timeout.tv_usec = timeout_usec;
-      ret=select (FD_SETSIZE,&fds, NULL, NULL,&timeout);
+      ret=select (FD_SETssize,&fds, NULL, NULL,&timeout);
       if (ret==1) {
-         printf("\r Comunicacion establecida \n");
-         n=read (serial_fd, &data, v.tamanio); 
+         printf("\r Communication established \n");
+         n=read (serial_fd, &data, v.ssize); 
          printf("   n = %d\n", n);
          printf("data = %p\n ", data);
          count+=1;
          data=0;
          }
-      }while (count<size && ret==1);
+      }while (count<ssize && ret==1);
    return n;
    }
 
-float *hilo_temp(int adc){
+float *tthread_temp(int adc){
    struct timespec next;
    clock_gettime(CLOCK_REALTIME, &next);
-   next.tv_sec = next.tv_sec + 0.01; // 10 miliseg
+   next.tv_sec = next.tv_sec + 0.01; // 10 ms
    next.tv_nsec = 0;
    clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &next, NULL);
    printf("\r adc = \n",adc);
@@ -100,33 +100,33 @@ float *hilo_temp(int adc){
    }
 
 main(){
-   int x; //valor leido del adc
-   unsigned int p = 0x01; //indica 1 salida
-   float res; //valor temp ºC
+   int x; //adc variable
+   unsigned int p = 0x01; //indicates 1 output
+   float res; //temp value in ºC 
    printf("\r \n");
-   if(ioperm(paralelo,1,1)){ //dar permisos pto paralelo
-      perror("ERROR: No tiene los permisos necesarios.");
+   if(ioperm(parallel,1,1)){ //parallel port permission
+      perror("ERROR: Not allowed.");
       exit(1);
       }
-   pthread_t serie; //llama al hilo que abre el puerto
-   pthread_create(&serie, NULL, &hilo_serie, NULL);
+   pthread_t serie; //calls tthread openning the port
+   pthread_create(&serie, NULL, &tthread_serie, NULL);
    pthread_join(serie, &v.fd);
-   pthread_t config; //llama al hilo que config el puerto
-   pthread_create(&config, NULL, &hilo_config, NULL);
+   pthread_t config; //calls tthread configuring the port
+   pthread_create(&config, NULL, &tthread_config, NULL);
    pthread_join(config, NULL);
    while(1){
-      pthread_t leer; //llama al hilo que leer el puerto
-      pthread_create(&leer, NULL, &hilo_leer, &v);
+      pthread_t leer; //calls tthread reading the port
+      pthread_create(&leer, NULL, &tthread_leer, &v);
       pthread_join(leer, &x);
-      pthread_t celsius; //llama al hilo que convierte el dato
-      pthread_create(&celsius, NULL, &hilo_temp, &x);
+      pthread_t celsius; //calls tthread converting the data
+      pthread_create(&celsius, NULL, &tthread_temp, &x);
       pthread_join(celsius, &res);
       printf("\r x = %d \n",x);
-      if(res>20){ //mayor a 20 C, prender el compresor
-         outb(p,paralelo); // salida del puerto (ON)
+      if(res>20){ //if over 20 C, turn on compressor (power switching)
+         outb(p,parallel); // port output (ON)
          }
-      if(res<10){ //menor a 10 C, apagar compresor
-         outb(0,paralelo); // salida del puerto (OFF)         
+      if(res<10){ //if below 10 C, shutdown compressor (power switching
+         outb(0,parallel); // puerto output (OFF)         
          }
 
       printf("\r \n");
